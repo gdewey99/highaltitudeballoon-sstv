40bd8056d7417f292819fdaffb96d435cda3a7db
gdewey99 <gdewey99@gmail.com>
2019-08-19 10:54:41 -0400

    Update newsstv.sh

--- newsstv.sh
+++ newsstv.sh
@@ -4,21 +4,15 @@ export SSTVDIR=/home/pi/SSTV
 cd $SSTVDIR
 
 # Set some variables
-GPIO_PTT=4
-#GPIO_PWR=11
-DEBUG=1
+GPIO_PTT=7
 
 # Initalize the GPIO ports 
-#/usr/local/bin/gpio mode $GPIO_PWR out
 /usr/local/bin/gpio mode $GPIO_PTT out 
-
-/usr/local/bin/gpio write $GPIO_PTT 0
-sleep 1
-/usr/local/bin/gpio write $GPIO_PTT 0
+/usr/local/bin/gpio write $GPIO_PTT 1
 
 # Set volume
-amixer sset PCM 100%
-
+#amixer sset PCM 100%
+#load camera module
 sudo modprobe bcm2835-v4l2
 
 ####################################
@@ -28,8 +22,8 @@ sudo modprobe bcm2835-v4l2
 #/usr/bin/fswebcam  --jpeg 95 --skip 2 -F 2 --deinterlace  --banner-colour "#10000000" --shadow --title "KD2PYB" #--crop 320x240  -d v4l2:/dev/video0 --save ${SSTVDIR}/webcam.jpg
 #/usr/bin/fswebcam  --no-banner --resolution 2592x1944 --png 0  -d v4l2:/dev/video0 ${SSTVDIR}/webcam2.png
 
-raspistill -t 1 -ex sports --width 320 --height 256 -e png -o /image.png
-raspistill -t 1 -ex sports --width 2592 --height 1944 -e jpg -o /image.jpg
+raspistill -t 1000 -ex sports --width 320 --height 256 -e png -o /image.png
+raspistill -t 1000 -ex sports --width 2592 --height 1944 -e jpg -o /image.jpg
 # add callsign
 #mogrify -pointsize 24 -draw "text 10,40 'KD2PYB'" /image.png
 convert image.png -font helvetica -fill white -stroke white -pointsize 24 -annotate +30+475 'KD2PYB' image.png
@@ -47,8 +41,8 @@ convert image.png -font helvetica -fill white -stroke white -pointsize 24 -annot
 #/usr/local/bin/gpio write $GPIO_PWR 1 
 #sleep 3
 # PTT ON
-/usr/local/bin/gpio write $GPIO_PTT 1 
-#sleep .25
+/usr/local/bin/gpio write $GPIO_PTT 0 
+sleep .5
 
 
 #
@@ -56,11 +50,8 @@ convert image.png -font helvetica -fill white -stroke white -pointsize 24 -annot
 #
 /usr/bin/aplay -d 0  ${SSTVDIR}/image.wav
 
-#########################
-/usr/local/bin/gpio mode $GPIO_PTT out 
-/usr/local/bin/gpio write $GPIO_PTT 0
 sleep 1
-/usr/local/bin/gpio write $GPIO_PTT 0
+/usr/local/bin/gpio write $GPIO_PTT 1
 ### SWITCH OFF WALKIE ###
 #########################
 sleep 170
@@ -77,8 +68,3 @@ sleep 170
 
  rm -f ${SSTVDIR}/image.wav
 
-#######################
-### Relaunch System ###
-#######################
-
-./sstv.sh
